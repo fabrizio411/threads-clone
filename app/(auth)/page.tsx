e9cec3fa59fae5c8d36f749711049a167d6d00bb
fabrizio411 <fabriziomourglia@gmail.com>
2023-09-12 12:59:59 -0300

    Set up and auth bg image

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+import './style.scss'
+
+const AuthPage = () => {
+  return (
+    <main className='page auth-page'>
+      <div className='bg-image'></div>
+    </main>
+  )
+}
+
+export default AuthPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import '@/sass/reset.scss'
+import '@/sass/global.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
